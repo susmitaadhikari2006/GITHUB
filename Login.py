b7c43f930c1bb43990ece86d00451a93a03e9585
Susmita Adhikari <susmitaadhikari2006@gmail.com>
2024-01-31 12:47:13 -0500

    hmm

--- Login.py
+++ Login.py
@@ -1,10 +1,30 @@
 import json
+users = []
+passWrd =[]
 
+with open('users.json', 'r') as file:
+    # Load JSON data from file
+    data = json.load(file)
+
+def getUser(datas):
+    for i in datas["username"]:
+        users.append(i)
+    return users
+def getPass(datas):
+    for i in datas["password"]:
+        passWrd.append(i)
+    return passWrd
+
+print(users)
+print(passWrd)
+
+# Closing file
 invalidInput = False
 special_characters = "!@#$%^&*()-+?_=,<>/ "
 Numbers = "1234567890"
 letters = "qwertyuiopasdfghjklzxcvbnm"
 #method to check password in the password
+print(getUser(data))
 def checkPass(str):
     # if password contains a special character returns true
     if (any(c in special_characters for c in str) and any(a in Numbers for a in str) and any(b in letters for b in str) and any(d in letters.upper() for d in str)):
@@ -20,8 +40,7 @@ while((not invalidInput)  and (n<5)):
     if(username.__contains__(" ") or not checkPass(password)):# does the username contains spaces, If so -> LOOP
         print("Username or Password not formated correctly, Try Again:")
         invalidInput = False
-    else:#meet requirements -> go to checking for if user and password match
-        #PUT USER AND PASSWORD VALIDATION HERE
+    else:
         invalidInput = True 
     n +=1
     if (n<=4):
