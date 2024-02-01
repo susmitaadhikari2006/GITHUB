728817513f0ca7828d4e13d0dff9d772a23584c1
Susmita Adhikari <susmitaadhikari2006@gmail.com>
2024-02-01 14:06:08 -0500

    school

--- word/wordle.py
+++ word/wordle.py
@@ -1,6 +1,13 @@
 Secret_word = "happy"
 i=0
+dashes = ""
 
+# putting in dashes for each letter
+for g in Secret_word:
+    dashes += "-"
+
+print(dashes)
+    
 def get_guess():
     global i
     invalidInput = False
@@ -18,7 +25,10 @@ def get_guess():
 while((i<10)):
     wordUser = get_guess()
     if (any(c in Secret_word for c in wordUser)):
-        print("this letter is in the word!")
+        print("this letter is in the word!\n")
+        dashes += wordUser +"-"  
+        print(dashes) 
     else:
         print("YOUR AN IDIOT, TRY AGAIN!")
+        print(dashes)
     i+=1
