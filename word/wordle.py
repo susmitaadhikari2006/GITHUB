a3f3013d73a01f9da2613870f3de17062805af03
Susmita Adhikari <susmitaadhikari2006@gmail.com>
2024-02-02 11:50:40 -0500

    Update wordle.py

--- word/wordle.py
+++ word/wordle.py
@@ -13,7 +13,7 @@ def get_guess():
     invalidInput = False
     letters = "qwertyuiopasdfghjklzxcvbnm"
     while((not invalidInput) and i<10):
-        letter = input("Enter your word(enter a letter):").lower() #not case specific
+        letter = input("Guess:").lower() #not case specific
         if(len(letter)>1 or (not any(b in letters for b in letter))):
             invalidInput = False
             print("you entered more then one letter or a number, Try Again")
@@ -26,8 +26,7 @@ while((i<10)):
     wordUser = get_guess()
     if (any(c in Secret_word for c in wordUser)):
         print("this letter is in the word!\n")
-        dashes += wordUser +"-"  
-        print(dashes) 
+        
     else:
         print("YOUR AN IDIOT, TRY AGAIN!")
         print(dashes)
