2ff6b895c23c7356e1ba6fc14ee73d6084e3ab69
Susmita Adhikari <susmitaadhikari2006@gmail.com>
2024-02-07 12:56:05 -0500

    Wordle phase 3

--- word/wordle.py
+++ word/wordle.py
@@ -1,9 +1,9 @@
 from colorama import Fore
 import random
 
-Secret_word = "software"
-i=0
+Secret_word = "hello"
 dashes = ""
+guesses_left = 10
 
 # putting in dashes for each letter
 for g in Secret_word:
@@ -15,29 +15,37 @@ def update_dashes(hidden_text, guessed_letters):
     return dashes
         
 def get_guess():
-    global i
     invalidInput = False
+    global guesses_left
     letters = "qwertyuiopasdfghjklzxcvbnm"
-    while((not invalidInput) and i<10):
+    while((not invalidInput) or (guesses_left<0)):
         letter = input("Guess:").lower() #not case specific
         if(len(letter)>1 or (not any(b in letters for b in letter))):
             invalidInput = False
             print("you entered more then one letter or a number, Try Again")
-            i+=1
+            guesses_left -= 1
+            print(str(guesses_left) + " incorrect guesses left.")
         else:
             invalidInput = True
     return letter
 
 guessed_letters = {""}
-while((i<10)):
+rightword = True
+while((rightword) and (guesses_left>0)):
     wordUser = get_guess()
     if (any(c in Secret_word for c in wordUser)):
         print("this letter is in the word!\n")
         guessed_letters.add(wordUser)
-        print(update_dashes(Secret_word, guessed_letters))
+        dashes = update_dashes(Secret_word, guessed_letters)
+        print(dashes)
+        if(dashes == Secret_word):
+            rightword = False   
     else:
         print("YOUR AN IDIOT, TRY AGAIN!")
+        guesses_left -= 1
         print(update_dashes(Secret_word, guessed_letters))
-    i+=1
-    if i==10:
-        print("The Word was: " + Secret_word)
+    print(str(guesses_left) + " incorrect guesses left.")
+if(guesses_left == 0):
+    print("you lost The word was: "+ Secret_word)
+else:
+    print("Congrats! You win. The word was: " + Secret_word)
