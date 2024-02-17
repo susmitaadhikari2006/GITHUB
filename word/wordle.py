e3d97b0b9a8b02ab4d204dc41c96b125f056b724
susmitaadhikari2006 <115734117+susmitaadhikari2006@users.noreply.github.com>
2024-02-17 18:03:15 -0500

    Wordle done

--- word/wordle.py
+++ word/wordle.py
@@ -1,14 +1,12 @@
 import random
 import os
-from colorama import Fore
+from colorama import Fore, Style
 
 wordList = []
 for x in open("words.txt", "r"):
     wordList.append(x.strip())
 Secret_word = wordList[random.randint(0,5753)]
-print(Secret_word)
-dashes = Secret_word
-guesses_left = 5
+guesses_left = 6
     
 def get_guess():
     invalidInput = False
@@ -17,25 +15,34 @@ def get_guess():
         letter = input("Guess:").lower() #not case specific
         if(not len(letter)==5 or not letter.isalpha() or not letter in wordList):
             invalidInput = False
-            print("Invalid Input, Please enter a 5 LETTER word")
+            print("Invalid Input, Please enter a 5 LETTER word, that is in the word list")
         else:
             invalidInput = True
     return letter
 
 def update_dashes(hidden_text, guessed_word):
-    # dashes = "".join(x if x.lower() in guessed_word else "-" for x in hidden_text)
-    guess[count] = guessed_word
-    return guessed_word
+    word = ""
+    for i in range(len(hidden_text)):
+        if guessed_word[i] == hidden_text[i]:
+            word += Fore.GREEN + guessed_word[i] + Style.RESET_ALL
+        elif guessed_word[i] in hidden_text:
+            word += Fore.YELLOW + guessed_word[i] + Style.RESET_ALL
+        else:
+            word += guessed_word[i]
+    return word
 
 rightword = True
 attempts = 0
 while((rightword) and (guesses_left>0)):
     wordUser = get_guess()
-    attempts =+ 1
+    guesses_left -= 1
+    attempts += 1
+    if(wordUser == Secret_word):
+        rightword = False
+    if(guesses_left==0):
+        rightword = False
     dashes = update_dashes(Secret_word, wordUser)
     print(str(attempts) + ". " + dashes)
-    if(dashes == Secret_word):
-        rightword = False
-    elif(guesses_left==0):
-        rightword = False
-    guesses_left -= 1
+
+if guesses_left == 0:
+    print("word was: " + Secret_word)
