67001b96198bb0d51644fc8f88f8f1f8d3a11069
susmitaadhikari2006 <115734117+susmitaadhikari2006@users.noreply.github.com>
2024-02-22 18:51:58 -0500

    image recon

--- image.py
+++ image.py
@@ -7,6 +7,7 @@ def draw_vertical_line(image):
     middle_x = width // 2
     # Draw the vertical line
     cv2.line(image, (middle_x, 0), (middle_x, height), (0, 0, 255), 2)
+    return middle_x
     
     
 cam = cv2.VideoCapture(0)
@@ -26,7 +27,7 @@ while True:
     
     # Find contours in the mask
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    draw_vertical_line(frame)
+    middle = draw_vertical_line(frame)
     
     
     # Draw bounding rectangles around detected orange items
@@ -36,9 +37,16 @@ while True:
         height = h
         area = width * height
         ratio = w/h
+        height_s, width_s, _ = frame.shape
         if((width >= height*2) and (area > 3000)):
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.putText(frame, f'Width: {width}, Height: {height}, Area: {area}', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+            # Draw a circle at the centroid
+            cv2.circle(frame, (height, width), 7, (255, 255, 255), -1)
+                
+             # Calculate distance between the two points (center of screen and centroid)
+            distance = numpy.sqrt((height_s - width_s // 2) ** 2 + (width_s - height // 2) ** 2)
 
     cv2.imshow('FRC Image Processing', frame)
 
