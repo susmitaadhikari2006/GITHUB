1f8fe240b77c42c23bdb2c01c6d4e71ee080e9f6
Susmita Adhikari <susmitaadhikari2006@gmail.com>
2024-03-01 12:30:30 -0500

    asdfghjk

--- DataBase/DataConnection.py
+++ DataBase/DataConnection.py
@@ -1,8 +1,30 @@
 import pyodbc
 
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=eccLABS.accdb')
+driver = '{Microsoft Access Driver (*.mdb, *.accdb)}'
+filePath = r'C:\Users\PS24Sadhikari\python\GITHUB\DataBase\ECCLabs.accdb'
+myDataSource = pyodbc.dataSources()
+access_driver = myDataSource['MS Access Database']
+
+conn = pyodbc.connect(driver = access_driver, dbq = filePath, autocommit=True)
+
 cursor = conn.cursor()
-cursor.execute('select * from instructors')
+cursor.execute('select * from tblLabPrograms')
+
+n=1
+print("\n Labs Information: \n")
+for row in cursor.fetchall():
+    print(str(n) + ". " + str(row))
+    n+=1
+    cursor = conn.cursor()
+
+print("\n Instructor Information: \n")
+cursor.execute('select firstName, lastName, Email  from tbl_instructors')
    
+for row in cursor.fetchall():
+    print(row)
+
+print("\n query Information: \n")
+
+cursor.execute('SELECT tblLabPrograms.Abbrev, tblLocations.locationName FROM tblLocations INNER JOIN tblLabPrograms ON (tblLocations.Abbreviation = tblLabPrograms.Building) AND (tblLocations.Abbreviation = tblLabPrograms.Building);')
 for row in cursor.fetchall():
     print(row)
